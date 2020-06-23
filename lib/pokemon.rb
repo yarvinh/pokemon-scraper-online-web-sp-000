204755eb34e5b7d036d65ccd77a01de314cd22fe
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-06-23 21:05:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_accessor :name, :type, :db
   attr_reader :id
-  def initialize(:id, :name, :type, :db)
+  def initialize(id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
